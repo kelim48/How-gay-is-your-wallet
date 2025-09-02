5628e82e5a5e4e8d3927e1702f3b65224d1755bf
kelim48 <kelim48@icloud.com>
2025-09-02 14:14:35 +0100

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  DONATION_ADDRESS: "2BA85mMDfBjJtNFrjnRVyRiwgspXPUVgG6FZEfwGU34F",
-  API_BASE: "https://back-end-el5q.onrender.com" // set your backend URL here after deploying server
-};
