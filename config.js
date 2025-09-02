b60373a7f76ab0df154d11548e6e02d2cdaa985a
kelim48 <kelim48@icloud.com>
2025-09-02 14:21:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   DONATION_ADDRESS: "2BA85mMDfBjJtNFrjnRVyRiwgspXPUVgG6FZEfwGU34F",
-  API_BASE: "https://back-end-el5q.onrender.com" // set your backend URL here after deploying server
+  API_BASE: "https://back-end-1-q9d4.onrender.com" // set your backend URL here after deploying server
 };
